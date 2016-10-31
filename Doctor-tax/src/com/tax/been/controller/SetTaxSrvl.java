a9107de94f8a68f322cb2524231d8aa1c830b52a
Natdanai <teayvvv5@gmail.com>
2016-10-31 09:51:53 +0700

    controller

---
+++ Doctor-tax/src/com/tax/been/controller/SetTaxSrvl.java
@@ -0,0 +1,67 @@
+package com.tax.been.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.tax.bean.util.DbConnector;
+
+/**
+ * Servlet implementation class SetTaxSrvl
+ */
+@WebServlet("/SetTaxSrvl")
+public class SetTaxSrvl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SetTaxSrvl() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getParameter("method").equals("genRoleGroup")){
+			response.setContentType("application/html");
+			response.setCharacterEncoding("UTF-8");
+			PrintWriter out = response.getWriter();
+			DbConnector db = new DbConnector();
+			db.doConnect();
+			String sql = "SELECT * FROM group_tax";
+			String option = "";
+			try{
+				ArrayList<HashMap<String,String>> listData = db.getData(sql);
+				
+					for(int i = 0 ; i < listData.size() ;i++){
+						String value = listData.get(i).get("id_group");
+						String name = listData.get(i).get("name_group");
+						option += "<option value=\""+value+"\">"+name+" </option>";
+					}
+				
+			}catch(Exception e){}
+			
+			System.out.println(option);
+			out.println(option);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
